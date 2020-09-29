df441bb6699b8550001101986df1983acf56a9c6
FAcorreiaa <fernandocorreia316@gmail.com>
2020-09-29 18:01:05 +0100

    Updates

---
+++ precache-manifest.00da29129e2872ce6ed4f12ebc68ba48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c75e23823c25ac349582c653e9fd99",
+    "url": "/webportfolio/index.html"
+  },
+  {
+    "revision": "db34ccac88e3e850f9af",
+    "url": "/webportfolio/static/css/main.b761dd04.chunk.css"
+  },
+  {
+    "revision": "3998e165aaece1b9e358",
+    "url": "/webportfolio/static/js/2.4d27b4cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/webportfolio/static/js/2.4d27b4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db34ccac88e3e850f9af",
+    "url": "/webportfolio/static/js/main.5423285e.chunk.js"
+  },
+  {
+    "revision": "b5a809cb52102889eecc",
+    "url": "/webportfolio/static/js/runtime-main.5343d6ab.js"
+  },
+  {
+    "revision": "d8ba360162468a4425ac57f923ae5b8b",
+    "url": "/webportfolio/static/media/food.d8ba3601.jpg"
+  },
+  {
+    "revision": "8636e4bd8e0499032d722fd33391ee82",
+    "url": "/webportfolio/static/media/resume.8636e4bd.png"
+  }
+]);
